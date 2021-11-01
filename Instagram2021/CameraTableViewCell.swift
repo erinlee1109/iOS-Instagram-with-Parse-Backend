bd7d6b2ee48669fe8f56d76acf1b120d6d3c8ea4
Yujeong Lee <erinlee1109@outlook.com>
2021-11-01 17:58:18 -0400

    going to try creating a nib file for any kind of image upload

---
+++ Instagram2021/CameraTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CameraTableViewCell.swift
+//  Instagram2021
+//
+//  Created by Yujeong Lee on 11/1/21.
+//
+
+import UIKit
+
+class CameraTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
